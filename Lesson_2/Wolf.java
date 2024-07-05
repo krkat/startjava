6ef743b8329e26a0c5ee1d70894b1615558ed650
krkat <98359411+EmmaFranBengtsbo@users.noreply.github.com>
2024-07-05 16:21:35 +0300

    Correct style in Wolf class

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -5,6 +5,50 @@ public class Wolf {
     private int age;
     private String color;
 
+    public boolean isMale() {
+        return isMale;
+    }
+
+    public void setMale(boolean isMale) {
+        this.isMale = isMale;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
     public void go() {
         System.out.println("Идет.");
     }
@@ -23,49 +67,5 @@ public class Wolf {
 
     public void hunt() {
         System.out.println("Охотится.");
-    }
-
-    public boolean isMale() {
-        return isMale;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public float getWeight() {
-        return weight;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setIsMale(boolean isMale) {
-        this.isMale = isMale;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setWeight(float weight) {
-        this.weight = weight;
-    }
-
-    public void setAge(int age) {
-        if (age > 8) {
-            System.out.println("Некорректный возраст");
-        } else {
-            this.age = age;
-        }
-    }
-
-    public void setColor(String color) {
-        this.color = color;
     }
 }
