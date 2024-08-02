b078b8459e01c1d309cfb43de43ebad3e7bb185d
krkat <98359411+EmmaFranBengtsbo@users.noreply.github.com>
2024-08-02 22:00:26 +0300

    Refactor FactorialElements

--- src/com/startjava/lesson_2_3/array/FactorialElements.java
+++ src/com/startjava/lesson_2_3/array/FactorialElements.java
@@ -4,52 +4,52 @@ import java.util.Arrays;
 
 public class FactorialElements {
     public static void main(String[] args) {
-        outputFactorial();
-        outputFactorial(null);
-        outputFactorial(8, 0, 9);
-        outputFactorial(-3, 1, 7, 13);
-        outputFactorial(-22, -0);
+        calcFactorial();
+        Integer[] a = null;
+        calcFactorial(a);
+        calcFactorial(8, 0, 9);
+        calcFactorial(-3, 1, 7, 13);
+        calcFactorial(-22, -0);
     }
 
-    private static void outputFactorial(Integer... args) {
-        System.out.print("\nИсходный иассив: ");
-        output(args);
+    private static void calcFactorial(Integer... ints) {
+        System.out.print("\nИсходный массив: ");
+        output(ints);
+        calc(ints);
+    }
+
+    private static void output(Integer... ints) {
+        if (ints == null) {
+            System.out.println("Ошибка! Массив null");
+            return;
+        }
+        if (ints.length == 0) {
+            System.out.println("Ошибка! Массив нулевой длины");
+            return;
+        }
+        System.out.println(Arrays.toString(ints));
+    }
+
+    private static Integer[] calc(Integer... ints) {
+        if (ints == null || ints.length == 0) {
+            return null;
+        }
         System.out.println("Вычисление факториалов элементов: ");
-        output(count(args));
-    }
-
-    private static void output(Integer... args) {
-        if (args == null) {
-            System.out.println("null");
-            return;
-        }
-        if (args.length == 0) {
-            System.out.println("[]");
-            return;
-        }
-        System.out.println(Arrays.toString(args));
-    }
-
-    private static Integer[] count(Integer... args) {
-        if (args == null || args.length == 0) {
-            System.out.println("Невозможно вычислить факториал элементов, т.к. массив null или нулевой длины");
-            return args;
-        }
-        int length = args.length;
+        int length = ints.length;
         Integer[] result = new Integer[length];
         for (int i = 0; i < length; i++) {
-            if (args[i] < 0) {
-                System.out.println("Ошибка: факториал " + args[i] + "! не определен");
+            if (ints[i] < 0) {
+                System.out.println("Ошибка: факториал " + ints[i] + "! не определен");
                 result[i] = null;
                 continue;
             }
-            System.out.print(args[i] + "! = ");
+            System.out.print(ints[i] + "! = ");
             result[i] = 1;
-            if (args[i] != 0 && args[i] != 1) {
-                for (int j = 1; j <= args[i]; j++) {
+            if (ints[i] != 0 && ints[i] != 1) {
+                for (int j = 1; j <= ints[i]; j++) {
                     System.out.print(j);
                     result[i] *= j;
-                    System.out.print(j != args[i] ? " * " : " = ");
+                    System.out.print(j != ints[i] ? " * " : " = ");
                 }
             }
             System.out.println(result[i]);
