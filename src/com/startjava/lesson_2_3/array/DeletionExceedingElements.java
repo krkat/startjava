bf9b24274cefa5a91911674c294cfbf808f2c455
krkat <98359411+EmmaFranBengtsbo@users.noreply.github.com>
2024-08-06 19:00:09 +0300

    Refactor DeletionExceedingElements

--- src/com/startjava/lesson_2_3/array/DeletionExceedingElements.java
+++ src/com/startjava/lesson_2_3/array/DeletionExceedingElements.java
@@ -1,69 +1,62 @@
 package com.startjava.lesson_2_3.array;
 
-import java.util.Arrays;
-
 public class DeletionExceedingElements {
     public static void main(String[] args) {
-        double[] doubles = new double[15];
-        initialize(doubles);
-        for(int indexMaxValue : new int[]{-1, 15, 0, 14}) {
-            deleteExceed(doubles, indexMaxValue);
+        float[] randomFloats = new float[15];
+        initialize(randomFloats);
+        for (int indexMaxValue : new int[]{-1, 15, 0, 14}) {
+            deleteExceed(randomFloats, indexMaxValue);
         }
     }
 
-    private static void initialize(double[] doubles) {
-        for (int i = 0; i < doubles.length; i++) {
-            doubles[i] = Math.random();
+    private static void initialize(float[] randomFloats) {
+        for (int i = 0; i < randomFloats.length; i++) {
+            randomFloats[i] = (float) Math.random();
         }
     }
 
-    private static void deleteExceed(double[] original, int indexMaxValue) {
+    private static void deleteExceed(float[] original, int indexMaxValue) {
         System.out.println("\nИсходный массив: ");
         customOutput(original);
         deleteExceededElements(original, indexMaxValue);
     }
 
-    private static void customOutput(double[] doubles) {
-        double[] part1 = Arrays.copyOfRange(doubles, 0, 8);
-        double[] part2 = Arrays.copyOfRange(doubles, 8, 15);
+    private static void customOutput(float[] doubles) {
         System.out.print("[");
-        formattedOutput(part1);
-        System.out.println();
-        formattedOutput(part2);
-        System.out.println("]");
-    }
-
-    private static void formattedOutput(double[] doubles) {
         for (int i = 0; i < doubles.length; i++) {
             System.out.printf("%.3f", doubles[i]);
             System.out.print(i != doubles.length - 1 ? ", " : "");
+            if (i == 7) {
+                System.out.println();
+            }
         }
+        System.out.println("]");
     }
 
-    private static void deleteExceededElements(double[] original, int indexMaxValue) {
+    private static void deleteExceededElements(float[] original, int indexMaxValue) {
         System.out.println("Вычисление результирующего массива: ");
-        if (checkIndexOutOfBounds(indexMaxValue, original.length)) {
+        if (isOutOfBounds(indexMaxValue, original.length)) {
             System.out.println("Результирующий массив не определен");
             return;
         }
-        double maxValue = original[indexMaxValue];
+        float maxValue = original[indexMaxValue];
         System.out.printf("Значение ячейки с индексом [%d]: %.3f%n", indexMaxValue, maxValue);
         int counterSetZero = 0;
-        double[] result = Arrays.copyOf(original, original.length);
-        for (int i = 0; i < result.length; i++) {
-            if (result[i] > maxValue) {
-                result[i] = 0;
+        for (int i = 0; i < original.length; i++) {
+            if (original[i] > maxValue) {
+                original[i] = 0;
                 counterSetZero++;
             }
         }
         System.out.println("Количество обнуленных ячеек: " + counterSetZero);
         System.out.println("Результирующий массив:");
-        customOutput(result);
+        customOutput(original);
     }
 
-    private static boolean checkIndexOutOfBounds(int index, int length) {
+    private static boolean isOutOfBounds(int index, int length) {
         if (index < 0 || index >= length) {
-            System.out.println("Индекс [" + index + "] " + (index < 0 ? "меньше 0" : "больше или равен " + length));
+            System.out.println("Индекс [" + index + "] " + (index < 0 ?
+                    "меньше 0" : "больше или равен " + length));
             return true;
         }
         return false;
