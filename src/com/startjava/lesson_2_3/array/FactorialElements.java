e5613cfa831b8228c318769c40fb13bd334f6fcc
krkat <98359411+EmmaFranBengtsbo@users.noreply.github.com>
2024-08-06 18:24:10 +0300

    Refactor FactorialElements

--- src/com/startjava/lesson_2_3/array/FactorialElements.java
+++ src/com/startjava/lesson_2_3/array/FactorialElements.java
@@ -5,55 +5,44 @@ import java.util.Arrays;
 public class FactorialElements {
     public static void main(String[] args) {
         calcFactorial();
-        Integer[] a = null;
+        int[] a = null;
         calcFactorial(a);
         calcFactorial(8, 0, 9);
         calcFactorial(-3, 1, 7, 13);
         calcFactorial(-22, -0);
     }
 
-    private static void calcFactorial(Integer... ints) {
-        System.out.print("\nИсходный массив: ");
-        output(ints);
-        calc(ints);
-    }
-
-    private static void output(Integer... ints) {
+    private static void calcFactorial(int... ints) {
         if (ints == null) {
-            System.out.println("Ошибка! Массив null");
+            System.out.println("\nОшибка! Исходный массив null");
             return;
         }
         if (ints.length == 0) {
-            System.out.println("Ошибка! Массив нулевой длины");
+            System.out.println("\nОшибка! Исходный массив нулевой длины");
             return;
         }
+        System.out.print("\nИсходный массив: ");
         System.out.println(Arrays.toString(ints));
+        calc(ints);
     }
 
-    private static Integer[] calc(Integer... ints) {
-        if (ints == null || ints.length == 0) {
-            return null;
-        }
+    private static void calc(int... ints) {
         System.out.println("Вычисление факториалов элементов: ");
-        int length = ints.length;
-        Integer[] result = new Integer[length];
-        for (int i = 0; i < length; i++) {
-            if (ints[i] < 0) {
-                System.out.println("Ошибка: факториал " + ints[i] + "! не определен");
-                result[i] = null;
+        for (int anInt : ints) {
+            if (anInt < 0) {
+                System.out.println("Ошибка: факториал " + anInt + "! не определен");
                 continue;
             }
-            System.out.print(ints[i] + "! = ");
-            result[i] = 1;
-            if (ints[i] != 0 && ints[i] != 1) {
-                for (int j = 1; j <= ints[i]; j++) {
+            System.out.print(anInt + "! = ");
+            int factorial = 1;
+            if (anInt != 0 && anInt != 1) {
+                for (int j = 1; j <= anInt; j++) {
                     System.out.print(j);
-                    result[i] *= j;
-                    System.out.print(j != ints[i] ? " * " : " = ");
+                    factorial *= j;
+                    System.out.print(j != anInt ? " * " : " = ");
                 }
             }
-            System.out.println(result[i]);
+            System.out.println(factorial);
         }
-        return result;
     }
 }
