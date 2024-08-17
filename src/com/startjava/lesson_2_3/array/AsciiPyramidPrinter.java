a7e6fd147f0d8d41daec03bd2adc40388a32cad5
krkat <98359411+EmmaFranBengtsbo@users.noreply.github.com>
2024-08-17 23:31:04 +0300

    Refactor AsciiPyramidPrinter

--- src/com/startjava/lesson_2_3/array/AsciiPyramidPrinter.java
+++ src/com/startjava/lesson_2_3/array/AsciiPyramidPrinter.java
@@ -8,9 +8,15 @@ public class AsciiPyramidPrinter {
     }
 
     private static void print(char start, char end, boolean asc) {
+        System.out.printf("Границы отрезка символов: '%c' с кодом %d, '%c' с кодом %d%n",
+                start, (int) start, end, (int) end);
+        System.out.printf("Сортировка по возрастанию от основания к вершине: %b%n%n", asc);
+        if (start > end) {
+            System.out.printf("Ошибка: левая граница (%d) > правой (%d)%n%n", (int) start, (int) end);
+            return;
+        }
         char[] symbols = init(start, end, asc);
-        int lines = symbols.length;
-        int widthPyramidBase = lines * 2 - 1;
+        int widthPyramidBase = symbols.length * 2 - 1;
         int numberLineSymbols = 1;
         int numberSpaces;
         for (char symbol : symbols) {
@@ -29,13 +35,7 @@ public class AsciiPyramidPrinter {
     }
 
     private static char[] init(char start, char end, boolean asc) {
-        if (start > end) {
-            char swap = start;
-            start = end;
-            end = swap;
-        }
-        int lines = Math.abs(end - start) + 1;
-        char[] symbols = new char[lines];
+        char[] symbols = new char[end - start + 1];
         for (char i = start; i <= end; i++) {
             symbols[i - start] = i;
         }
@@ -46,10 +46,11 @@ public class AsciiPyramidPrinter {
     }
 
     private static void reverse(char[] symbols) {
-        for (int length = symbols.length - 1; length >= symbols.length / 2; length--) {
-            char swap = symbols[length];
-            symbols[length] = symbols[symbols.length - length - 1];
-            symbols[symbols.length - length - 1] = swap;
+        int len = symbols.length;
+        for (int i = 0; i < len - 1; i++) {
+            char swap = symbols[i];
+            symbols[i] = symbols[--len];
+            symbols[len] = swap;
         }
     }
 }
