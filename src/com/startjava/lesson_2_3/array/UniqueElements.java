b8417fdac464177a07636b8457b4ac1d44928f4b
krkat <98359411+EmmaFranBengtsbo@users.noreply.github.com>
2024-08-17 23:13:08 +0300

    Refactor UniqueElements

--- src/com/startjava/lesson_2_3/array/UniqueElements.java
+++ src/com/startjava/lesson_2_3/array/UniqueElements.java
@@ -8,19 +8,26 @@ public class UniqueElements {
         print(-30, 10, 10);
         print(34, -34, 0);
         print(-1, 2, -3);
+        print(5, -8, 2);
     }
 
     private static void print(int start, int end, int elementsInLine) {
-        System.out.printf("Границы отрезка целых чисел: %d, %d%n", start, end);
+        System.out.printf("%nГраницы отрезка целых чисел: %d, %d%n", start, end);
         System.out.printf("Количество чисел, выводимых в строке консоли: %d%n", elementsInLine);
-        if (elementsInLine <= 1) {
-            System.out.printf("Ошибка: Количество чисел в строке не может быть меньше 1 (%d)%n%n",
-                    elementsInLine);
-            return;
+        boolean hasErrorsInputData = false;
+        if (start > end) {
+            System.out.printf("Ошибка: левая граница (%d) > правой (%d)%n", start, end);
+            hasErrorsInputData = true;
         }
-        int length = (int) ((start >= end ? start - end + 1 : end - start + 1) * 0.75);
+        if (elementsInLine <= 1) {
+            System.out.printf("Ошибка: Количество чисел в строке не может быть меньше 1 (%d)%n",
+                    elementsInLine);
+            hasErrorsInputData = true;
+        }
+        if (hasErrorsInputData) return;
+        int length = (int) ((end - start + 1) * 0.75);
         if (length <= 0) {
-            System.out.printf("Ошибка: Длина массива должна быть больше 0 (%d)%n%n", length);
+            System.out.printf("Ошибка: Длина массива должна быть больше 0 (%d)%n", length);
             return;
         }
         int[] uniqueInts = init(start, end, length);
@@ -29,13 +36,14 @@ public class UniqueElements {
     }
 
     private static int[] init(int start, int end, int length) {
+        int range = end - start + 1;
         int[] uniqueInts = new int [length];
         int random;
         boolean isRepeat;
         for (int i = 0; i < length; i++) {
             do {
                 isRepeat = false;
-                random = (int) (Math.random() * length) + (Math.min(start, end));
+                random = (int) (Math.random() * range) + start;
                 for (int j = 0; j < i; j++) {
                     if (uniqueInts[j] == random) {
                         isRepeat = true;
@@ -50,9 +58,14 @@ public class UniqueElements {
 
     private static void output(int[] ints, int elementsInLine) {
         Arrays.sort(ints);
-        for (int i = 0; i < elementsInLine; i++) {
-            System.out.print(ints[i] + " ");
+        int counter = 0;
+        for (int a : ints) {
+            if (counter == elementsInLine) {
+                counter = 0;
+                System.out.println();
+            }
+            System.out.printf("%3d ", a);
+            counter++;
         }
-        System.out.println();
     }
 }
