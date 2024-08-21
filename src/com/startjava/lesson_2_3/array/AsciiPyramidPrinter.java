9fd3d41f4fe16e1e0a99da1aba05a93798e9d28b
krkat <98359411+EmmaFranBengtsbo@users.noreply.github.com>
2024-08-21 17:50:31 +0300

    Refactor AsciiPyramidPrinter

--- src/com/startjava/lesson_2_3/array/AsciiPyramidPrinter.java
+++ src/com/startjava/lesson_2_3/array/AsciiPyramidPrinter.java
@@ -19,34 +19,25 @@ public class AsciiPyramidPrinter {
         int widthPyramidBase = symbols.length * 2 - 1;
         int numberLineSymbols = 1;
         int numberSpaces;
-        int s = asc ? end : start;
-        int e = asc ? start : end;
-        int increment = asc ? -1 : 1;
-        while (s != e) {
+        StringBuilder line;
+        for (char symbol : symbols) {
             numberSpaces = (widthPyramidBase - numberLineSymbols) / 2;
-            StringBuilder line = new StringBuilder();
-            for (int i = 0; i < numberSpaces; i++) {
-                line.append(" ");
-            }
-            for (int i = 0; i < numberLineSymbols; i++) {
-                line.append(symbols[s - start]);
-            }
+            line = new StringBuilder();
+            line.append(" ".repeat(numberSpaces));
+            line.append(String.valueOf(symbol).repeat(numberLineSymbols));
             System.out.println(line);
             numberLineSymbols += 2;
-            s += increment;
         }
-        StringBuilder line = new StringBuilder();
-        for (int i = 0; i < numberLineSymbols; i++) {
-            line.append(symbols[s - start]);
-        }
-        System.out.println(line);
         System.out.println();
     }
 
     private static char[] init(char start, char end, boolean asc) {
         char[] symbols = new char[end - start + 1];
+        int increment = asc ? -1 : 1;
+        int index = asc ? end - start : 0;
         for (char i = start; i <= end; i++) {
-            symbols[i - start] = i;
+            symbols[index] = i;
+            index += increment;
         }
         return symbols;
     }
