d0204c7e370f5e4b7958fa5e1d0a8e612cabca63
krkat <98359411+EmmaFranBengtsbo@users.noreply.github.com>
2024-09-04 20:44:16 +0300

    Refactor HW4 Calculator*

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -3,33 +3,34 @@ package com.startjava.lesson_2_3_4.calculator;
 public class Calculator {
     private static final int NUMBER_ELEMENTS = 3;
 
-    public static double calculate(String expression) throws RuntimeException {
+    public static double calculate(String expression) {
+        String[] elements = expression.split(" ");
+        if (elements.length != NUMBER_ELEMENTS) {
+            throw new RuntimeException("Ошибка: количество элементов выражения неверное.");
+        }
+        int x;
+        int y;
         try {
-            String[] elements = expression.split(" ");
-            if (elements.length != NUMBER_ELEMENTS) {
-                throw new RuntimeException("Ошибка: количество элементов выражения неверное.");
-            }
-            int x = Integer.parseInt(elements[0]);
-            String sign = elements[1];
-            int y = Integer.parseInt(elements[2]);
-
-            return switch (sign) {
-                case "+" -> x + y;
-                case "-" -> x - y;
-                case "*" -> x * y;
-                case "^" -> Math.pow(x, y);
-                case "/", "%" -> {
-                    if (y == 0) {
-                        throw new RuntimeException("Ошибка: Деление на 0 запрещено.");
-                    }
-                    yield divOrMod(x, y, sign);
-                }
-                default -> throw new RuntimeException("Ошибка: операция '" + sign + "' не поддерживается." +
-                            "\nДоступны следующие операции: +. -, *. /, ^. %");
-            };
+            x = Integer.parseInt(elements[0]);
+            y = Integer.parseInt(elements[2]);
         } catch (NumberFormatException e) {
             throw new RuntimeException("Ошибка: введены не целые числа");
         }
+        String sign = elements[1];
+        return switch (sign) {
+            case "+" -> x + y;
+            case "-" -> x - y;
+            case "*" -> x * y;
+            case "^" -> Math.pow(x, y);
+            case "/", "%" -> {
+                if (y == 0) {
+                    throw new RuntimeException("Ошибка: Деление на 0 запрещено.");
+                }
+                yield divOrMod(x, y, sign);
+            }
+            default -> throw new RuntimeException("Ошибка: операция '" + sign + "' не поддерживается." +
+                    "\nДоступны следующие операции: +. -, *. /, ^. %");
+        };
     }
 
     private static Double divOrMod(int x, int y, String sign) {
