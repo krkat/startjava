3e440a4435bed544bfa3760dd5dad25443586cad
krkat <98359411+EmmaFranBengtsbo@users.noreply.github.com>
2024-09-16 17:37:18 +0300

    Create Graduation project

---
+++ src/com/startjava/graduation/bookshelf/Book.java
@@ -0,0 +1,18 @@
+package com.startjava.graduation.bookshelf;
+
+public class Book {
+    private String author;
+    private String title;
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", year=" + year +
+                '}';
+    }
+
+    private int year;
+
+}
