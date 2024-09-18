d1f1ed0492fb0bbdc45b1457e791ab14a873d104
krkat <98359411+EmmaFranBengtsbo@users.noreply.github.com>
2024-09-18 09:18:39 +0300

    Clear main in BookshelfTest in master

--- src/com/startjava/graduation/bookshelf/BookshelfTest.java
+++ src/com/startjava/graduation/bookshelf/BookshelfTest.java
@@ -2,8 +2,6 @@ package com.startjava.graduation.bookshelf;
 
 public class BookshelfTest {
     public static void main(String[] args) {
-        System.out.println("Commit 1 in master");
-        System.out.println("Commit 1");
-        System.out.println("Commit 2");
+        
     }
 }
