79035d9784bf9927a587109e7903077739d2c65a
krkat <98359411+EmmaFranBengtsbo@users.noreply.github.com>
2024-10-05 15:52:12 +0300

    Create sql scripts for database robots

---
+++ src/com/startjava/lesson_5/populate.sql
@@ -0,0 +1,11 @@
+INSERT INTO jaegers (id, model_name, mark, height, weight, status, origin, launch, kaiju_kill)
+VALUES  (1, 'Gipsy Danger', 'Mark-3', 260, 1.98, 'Destroyed', 'United States ofAmerica', '2017-07-10', 9),
+        (2, 'Cherno Alpha', 'Mark-1', 280, 2.412, 'Destroyed', 'Russia', '2015-01-11', 6),
+        (3, 'Striker Eureka', 'Mark-5', 250, 41.792, 'Destroyed', 'Australia', '2019-11-02', 11),
+        (4, 'Coyote Tangp', 'Mark-1', 280, 2.312, 'Destroyed', 'Japan', '2015-12-30', 2),
+        (5, 'Crimson Typhoon', 'Mark-4', 250, 1.722, 'Destroyed', 'China', '2018-08-22', 7),
+        (6, 'Diablo Intercept', 'Mark-2', 270, 3.98, 'Destroyed', 'Chile', '2016-11-01', 5),
+        (7, 'Gipsy Avenger', 'Mark-6', 268, 45.270, 'Destroyed', 'United States of America', '2034-01-12', 1),
+        (8, 'Guardian Bravo', 'Mark-6', 240, 1.975, 'Active (But heavily damaged)', 'United States of America', '2035-12-15', 2),
+        (9, 'Storrm Garuda', 'Mark-7', 270, 1.95, 'Active', 'Indonesia', '2035-11-01', 1),
+        (10, 'Nova Hyperior', 'Mark-4', 230, 1.5, 'Destroyed', 'South Korea', '2018-11-01', 3);
